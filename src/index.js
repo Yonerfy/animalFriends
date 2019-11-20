af337228a462befa333b4f71643202d25c192d2b
Yonerfy <34351998+Yonerfy@users.noreply.github.com>
2019-11-20 06:41:35 +0100

    learning react :) I love this :)

--- src/index.js
+++ src/index.js
@@ -1,10 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import Card from './Card';
 import * as serviceWorker from './serviceWorker';
+import 'tachyons';
+import {animalFriends} from './animalFriends';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+    <div>
+        <Card id={animalFriends[0].id} name={animalFriends[0].name} email={animalFriends[0].email}/>
+        <Card id={animalFriends[1].id} name={animalFriends[1].name} email={animalFriends[1].email}/>
+        <Card id={animalFriends[2].id} name={animalFriends[2].name} email={animalFriends[2].email}/>
+    </div>
+    , document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
