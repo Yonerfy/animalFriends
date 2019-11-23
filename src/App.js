1e7f8fb564882b198969a30b4165520f13dc4375
Yonerfy <34351998+Yonerfy@users.noreply.github.com>
2019-11-23 09:06:09 +0100

    Here come the errors :)

--- src/App.js
+++ src/App.js
@@ -1,16 +1,35 @@
-import React from  'react';
+import React, {Component} from  'react';
 import CardList from './CardList';
 import Searchbox from './Searchbox';
 import {animalFriends} from './animalFriends';
 
-const App = () => {
-    return (
-        <div className ='tc'>
-            <h1>AnimalFriends</h1>
-            <Searchbox />
-            <CardList animalFriends={animalFriends} />
-        </div>
-    );
+
+
+class App extends Component {
+    constructor(){
+        super()
+        this.state = {
+            animalFriends: animalFriends,
+            searchfield: ''
+        }
+    }
+
+    onSearchChange = (event) => {
+        this.setState({ searchfield: event.target.value})
+        const filterdAnimals = this.state.animalFriends.filter(animalFriends => {
+            return animalFriends.name.toLowerCase().includes(this.state.searchfield.toLowerCase());
+        })
+    }
+
+    render() {
+        return (
+            <div className ='tc'>
+                <h1>AnimalFriends</h1>
+                <Searchbox searchChange={this.onSearchChange}/>
+                <CardList animalFriends={this.state.animalFriends} />
+            </div>
+        );
+    }
 }
 
 
--- src/Searchbox.js
+++ src/Searchbox.js
@@ -1,9 +1,10 @@
 import React from 'react';
 
-const Searchbox = () => {
+const Searchbox = ({searchfield,searchChange }) => {
     return (
         <div className='pa2'>
-            <input className='pa3 ba b--green bg-lightest-blue' type='search' placeholder='search animals'/>
+            <input className='pa3 ba b--green bg-lightest-blue' type='search'
+             placeholder='search animals' onChange={searchChange}/>
         </div>
     );
 }
